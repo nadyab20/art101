58ac2dfd9b4ceddb3ca0740b1bcae21f3ffd7913
nadyab20 <nbogusla@ucsc.edu>
2024-11-14 16:40:08 -0800

    ikk

---
+++ lab10/js/lab.js
@@ -0,0 +1,22 @@
+// Lab 10: Javascript for the Web 
+// Author: Nadya Boguslavsky <nbogusla@ucsc.edu>
+// Date: November 7, 2024 
+
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
+
+// click listener for button
+$("#make-convo").click(function(){
+    const newText = generateRandomText();
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+
+});
+
