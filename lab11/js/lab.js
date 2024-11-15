1f6c54ee690879cf493727c5f2de19bfb27975a6
nadyab20 <nbogusla@ucsc.edu>
2024-11-14 17:25:59 -0800

    adding files

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -10,13 +10,8 @@ function sortString(inputString) {
 
   // click listener for button
 $("#submit").click(function(){
+    const userName = $("#user-name").val();
+    userNameSorted = sortString(userName);
+    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
 });
-
-// get value of input field
-const userName = $("#user-name").val();
-
-// now let's sort it
-userNameSorted = sortString(userName);
-
-// append a new div to our output div
-$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
